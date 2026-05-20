63b3a88aa5021f2ffafbaffa916a2c687d7f4228
shivaverma6335-eng <shivaverma6335@gmail.com>
2026-05-20 17:46:48 +0000

    conversion

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -1,90 +1,133 @@
-let age = 33
+// let age = 33
 
-console.log(typeof age);
-console.log(typeof(age));
+// console.log(typeof age);
+// console.log(typeof(age));
 
-let score = "33"
+// let score = "33"
 
-let valueInnumber = Number(score)
-console.log(valueInnumber)
-console.log(typeof valueInnumber)
+// let valueInnumber = Number(score)
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber)
 
-if(true){
-    let score = "33abc"
-    let valueInnumber = Number(score)
-   console.log(valueInnumber)
-    console.log(typeof valueInnumber)
-}
-
-
-if(true){
-    let score = null
-    console.log(score)
-    let valueInnumber = Number(score)
-   console.log(valueInnumber)
-    console.log(typeof valueInnumber)
-}
+// if(true){
+//     let score = "33abc"
+//     let valueInnumber = Number(score)
+//    console.log(valueInnumber)
+//     console.log(typeof valueInnumber)
+// }
 
 
-
-if(true){
-    let score = undefined
-    console.log(score)
-    let valueInnumber = Number(score)
-   console.log(valueInnumber)
-    console.log(typeof valueInnumber)
-}
+// if(true){
+//     let score = null
+//     console.log(score)
+//     let valueInnumber = Number(score)
+//    console.log(valueInnumber)
+//     console.log(typeof valueInnumber)
+// }
 
 
 
-if(true){
-    let score = "shiva"
-    console.log(score)
-    let valueInnumber = Number(score)
-   console.log(valueInnumber)
-    console.log(typeof valueInnumber)
-}
-
-
-if(true){
-    let score = true
-    console.log(score)
-    let valueInnumber = Number(score)
-   console.log(valueInnumber)
-    console.log(typeof valueInnumber)
-}
-
-//   other datatype to number
-//       "33" => 33
-//       "33ab" => NaN(not an number)
-//       true => 1; false => 0
-
-
-      let isLoggedIn = 1
-      let boolIsLoggedIn3 = Boolean(isLoggedIn)
-         console.log(boolIsLoggedIn3)
-    console.log(typeof boolIsLoggedIn3)
+// if(true){
+//     let score = undefined
+//     console.log(score)
+//     let valueInnumber = Number(score)
+//    console.log(valueInnumber)
+//     console.log(typeof valueInnumber)
+// }
 
 
 
+// if(true){
+//     let score = "shiva"
+//     console.log(score)
+//     let valueInnumber = Number(score)
+//    console.log(valueInnumber)
+//     console.log(typeof valueInnumber)
+// }
 
-      let isLoggedIn = "shiva"
-      let boolIsLoggedIn3 = Boolean(isLoggedIn)
-         console.log(boolIsLoggedIn3)
-    console.log(typeof boolIsLoggedIn3)
+
+// if(true){
+//     let score = true
+//     console.log(score)
+//     let valueInnumber = Number(score)
+//    console.log(valueInnumber)
+//     console.log(typeof valueInnumber)
+// }
+
+// //   other datatype to number
+// //       "33" => 33
+// //       "33ab" => NaN(not an number)
+// //       true => 1; false => 0
 
 
-
-    // other to boolean
-        // 1 => true; 0 => false
-        // "" => false
-        // "Shiva" => true
+//       let isLoggedIn = 1
+//       let boolIsLoggedIn3 = Boolean(isLoggedIn)
+//          console.log(boolIsLoggedIn3)
+//     console.log(typeof boolIsLoggedIn3)
 
 
 
 
-let somenumber = 33
+//       let isLoggedIn = "shiva"
+//       let boolIsLoggedIn3 = Boolean(isLoggedIn)
+//          console.log(boolIsLoggedIn3)
+//     console.log(typeof boolIsLoggedIn3)
 
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+
+
+//     // other to boolean
+//         // 1 => true; 0 => false
+//         // "" => false
+//         // "Shiva" => true
+
+
+
+
+// let somenumber = 33
+
+// let stringnumber = String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+let value = 3
+let negValue = -value
+ //console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Shiva"
+ 
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2) 
+// console.log(1 + "2") 
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+
+let x = 3n;
+
+let y2 = x++
+console.log(x);
+console.log(y2);
+
+
+
