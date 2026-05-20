2bd4a2bfa1565f14546bc4a40fa0e546a905f743
shivaverma6335-eng <shivaverma6335@gmail.com>
2026-05-20 08:44:20 +0000

    conversion of datatype

---
+++ basic/02_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs ,notbrowser
+
+console.log(3
+    +
+    3) //code readibility should be high
+
+    console.log("Shiva")
+
+
+    let name = "shiva"
+    let age = 18
+    let isLoggedIn = false
+
+
+    // Data Type
+               //Primitive
+        //number => 2 t power 53
+        //bigint
+        //string => ""
+        // booleun => true/false
+        //null => standalone value
+        // undefined => abhi value nhi assign hui h 
+        //symbol => unique
+
+
+         //object
+
+
+console.log(typeof "Shiva");
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); // object
+
---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,90 @@
+let age = 33
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let score = "33"
+
+let valueInnumber = Number(score)
+console.log(valueInnumber)
+console.log(typeof valueInnumber)
+
+if(true){
+    let score = "33abc"
+    let valueInnumber = Number(score)
+   console.log(valueInnumber)
+    console.log(typeof valueInnumber)
+}
+
+
+if(true){
+    let score = null
+    console.log(score)
+    let valueInnumber = Number(score)
+   console.log(valueInnumber)
+    console.log(typeof valueInnumber)
+}
+
+
+
+if(true){
+    let score = undefined
+    console.log(score)
+    let valueInnumber = Number(score)
+   console.log(valueInnumber)
+    console.log(typeof valueInnumber)
+}
+
+
+
+if(true){
+    let score = "shiva"
+    console.log(score)
+    let valueInnumber = Number(score)
+   console.log(valueInnumber)
+    console.log(typeof valueInnumber)
+}
+
+
+if(true){
+    let score = true
+    console.log(score)
+    let valueInnumber = Number(score)
+   console.log(valueInnumber)
+    console.log(typeof valueInnumber)
+}
+
+//   other datatype to number
+//       "33" => 33
+//       "33ab" => NaN(not an number)
+//       true => 1; false => 0
+
+
+      let isLoggedIn = 1
+      let boolIsLoggedIn3 = Boolean(isLoggedIn)
+         console.log(boolIsLoggedIn3)
+    console.log(typeof boolIsLoggedIn3)
+
+
+
+
+      let isLoggedIn = "shiva"
+      let boolIsLoggedIn3 = Boolean(isLoggedIn)
+         console.log(boolIsLoggedIn3)
+    console.log(typeof boolIsLoggedIn3)
+
+
+
+    // other to boolean
+        // 1 => true; 0 => false
+        // "" => false
+        // "Shiva" => true
+
+
+
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
